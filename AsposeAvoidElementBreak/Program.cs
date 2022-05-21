a0dc63483625a7cd48cd54aaa51b4d3d0719ad9f
Joost <me@joost.rocks>
2022-05-21 11:22:18 +0200

    Add project files.

---
+++ AsposeAvoidElementBreak/Program.cs
@@ -0,0 +1,14 @@
+﻿using Aspose.Pdf;
+
+var pdfDocument = new Document();
+
+HtmlLoadOptions options = new HtmlLoadOptions();
+var html = "<div style=\"border: 10px solid red; break-inside: avoid; height: 200px\">dont break me </div><div style=\"border: 10px solid red; break-inside: avoid; height: 200px\">dont break me </div><div style=\"border: 10px solid red; break-inside: avoid; height: 200px\">dont break me </div><div style=\"border: 10px solid red; break-inside: avoid; height: 200px\">dont break me </div><div style=\"border: 10px solid red; break-inside: avoid; height: 200px\">dont break me </div>";
+            HtmlFragment textFragment = new HtmlFragment(html)
+            {
+                HtmlLoadOptions = options
+            };
+
+var page = pdfDocument.Pages.Add();
+page.Paragraphs.Add(textFragment);
+pdfDocument.Save("html_test.PDF");
